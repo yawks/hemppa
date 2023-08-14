4ae1c3636277f837e852dd2a24c98519a45eed08
Mat <4396128+yawks@users.noreply.github.com>
2023-08-14 23:56:28 +0200

    display DeepL error message in matrix

--- modules/translate.py
+++ modules/translate.py
@@ -4,6 +4,7 @@ from modules.common.module import BotModule
 
 USAGE = 'Usage: !translate <target_lang> <my message to translate>\nEg: !translate FR Hello, how are you?"'
 
+
 class MatrixModule(BotModule):
     def __init__(self, name):
         super().__init__(name)
@@ -15,13 +16,16 @@ class MatrixModule(BotModule):
         if len(args) < 3:
             await bot.send_text(USAGE)
         else:
-            target_lang = args[1]
-            translator = deepl.Translator(os.getenv('DEEPL_KEY', ''), server_url='https://api-free.deepl.com')
-            result = translator.translate_text(' '.join(args[2:]), target_lang=target_lang)
-            if isinstance(result, list):
-                await bot.send_text(room, result[0].text)
-            else:
-                await bot.send_text(room, result.text)
+            try:
+                target_lang = str(args[1]).upper()
+                translator = deepl.Translator(os.getenv('DEEPL_KEY', ''), server_url='https://api-free.deepl.com')
+                result = translator.translate_text(' '.join(args[2:]), target_lang=target_lang)
+                if isinstance(result, list):
+                    await bot.send_text(room, result[0].text)
+                else:
+                    await bot.send_text(room, result.text)
+            except deepl.DeepLException as deepl_exception:
+                await bot.send_text(room, 'Error during translation:\n\t' + str(deepl_exception))
 
     def help(self):
         return 'Translate what user has said\n' + USAGE
